813b3f234c610b4f04c26132970503706452f878
Rajesh Kumar Jena <jenarajeshkumar768@gmail.com>
2023-07-21 15:34:05 +0530

    finalized the deployment code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import SimpleInput from "./components/SimpleInput";
 
 import BasicForm from "./components/BasicForm";
-import Newform from "./components/Newform";
 
 function App() {
   return (
     <div className="app">
-      {/* <SimpleInput /> */}
+      <SimpleInput />
       {/* <BasicForm></BasicForm> */}
-      <Newform></Newform>
     </div>
   );
 }
--- src/components/Newform.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Newform = () => {
-  return <div>Newform</div>;
-};
-
-export default Newform;
